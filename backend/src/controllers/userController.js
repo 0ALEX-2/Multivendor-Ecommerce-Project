eb11bf7fc1df28437d69d20fa422e590a447495f
0ALEX-2 <sakilahmed965@gmail.com>
2024-11-02 22:42:42 +0530

    user apis

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -41,5 +41,66 @@ export const loginUser=expressAsyncHandler(async(req,res)=>{
     }
 })
 
+export const profile=expressAsyncHandler(async(req,res)=>{
+    const {_id}=req.body
 
-// 7 min
+    const user=await User.findById(_id)
+    if(user){
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role,
+            phone:user.phone,
+            isActive:user.isActive
+        })
+    }else{ 
+        throw new AppError("User not found!")
+    }
+})
+
+export const updateProfile=expressAsyncHandler(async(req,res)=>{
+    const {_id}=req.body
+
+    const user=await User.findById(_id)
+    if(user){
+       user.name=req.body.name || user.name;
+       user.email=req.body.email || user.email;
+       user.address=req.body.address || user.address;
+       user.phone=req.body.phone || user.phone;
+       if(req.body.password){user.password=req.body.password}
+
+       const updateUser=await User.save()
+
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role,
+            phone:user.phone,
+            isActive:user.isActive,
+            address:user.address
+        })
+    }else{ 
+        throw new AppError("User not found!")
+    }
+})
+
+export const getAllProfile=expressAsyncHandler(async(req,res)=>{
+    const users=await User.find()
+    if(users){
+        res.json(users)
+    }else{
+        throw new AppError("Users not found")
+    }
+})
+
+export const deleteUserProfile=expressAsyncHandler(async (req,res)=>{
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.json({ message: "User deleted successfully." })
+    } catch (error) {
+        throw new AppError("User not found!")
+    }
+})
+
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
-import {loginUser, registerUser} from "../controllers/userController.js"
+import {deleteUserProfile, getAllProfile, loginUser, profile, registerUser, updateProfile} from "../controllers/userController.js"
+import { authorize, protect } from "../middlewares/authMiddleware.js";
 
 const userRouter=express.Router()
 
 userRouter.post("/register",registerUser)
 userRouter.post("/login",loginUser)
+userRouter.get("/profile",protect,profile)
+userRouter.put("/profile",protect,updateProfile)
+userRouter.get("/users",protect,authorize("admin"),getAllProfile)
+userRouter.delete("/user/:id",protect,deleteUserProfile)
 
 
 export default userRouter
