fcc979ad4f563d4653e4f260747bf54164e67da6
0ALEX-2 <sakilahmed965@gmail.com>
2024-11-07 22:09:56 +0530

    review controller

---
+++ backend/src/controllers/reviewController.js
@@ -0,0 +1,57 @@
+import {Review} from "../models/reviewModel.js"
+import {AppError} from "../middlewares/errorHandler.js"
+import expressAsyncHandler from "express-async-handler"
+
+
+export const createReview=expressAsyncHandler(async(req,res)=>{
+try {
+    const newRevew=await Review.create(req.body)
+    res.status(201).json({status:true,data:newRevew})
+} catch (error) {
+    throw new AppError(error,400)
+}
+})
+
+
+export const getAllReview=expressAsyncHandler(async(req,res)=>{
+    try {
+        const reviews=await Review.find()
+        res.status(200).json({status:true,data:reviews})
+    } catch (error) {
+        throw new AppError(error,400)
+    }
+})
+
+export const getReviewById=expressAsyncHandler(async(req,res)=>{
+    try {
+        const review=await Review.findOne({id:req.params.id})
+        res.status(200).json({status:true,data:review})
+    } catch (error) {
+        throw new AppError(error,400)
+    }
+})
+
+export const updateReview=expressAsyncHandler(async(req,res)=>{
+    try {
+        const review=await Review.findByIdAndUpdate(req.params.id,req.body,{new:true})
+        if(!review){
+            throw new AppError("Review not found",404)
+        }
+        res.status(201).json({status:true,data:review})
+    } catch (error) {
+        throw new AppError(error,400)
+    }
+})
+
+export const deleteReview=expressAsyncHandler(async(req,res)=>{
+    try {
+        const review=await Review.findByIdAndDelete(req.params.id)
+        if(!review){
+            throw new AppError("Review not found!",404)
+        }
+        res.status(200).json({status:true,message:"Review deleted  successfully!"})
+
+    } catch (error) {
+       throw new AppError(error,400) 
+    }
+})
